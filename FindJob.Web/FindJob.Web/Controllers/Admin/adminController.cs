149581c2060674df7551410d1d434e453c5071b4
QiangBB <752865241@qq.com>
2018-07-14 11:19:35 +0800

    update

---
+++ FindJob.Web/FindJob.Web/Controllers/Admin/adminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindJob.Web.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /admin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ FindJob.Web/FindJob.Web/Controllers/Home/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindJob.Web.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
